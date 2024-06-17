799c7a81d986e1e0f0342dc85218f6a698441e5d
Milton Black <arziblack2@gmail.com>
2024-06-17 14:11:49 +0100

    feat: removed the team section from the footer, and added links to telegram and twitter in the footer section...

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,20 +22,36 @@ const Home = () => {
   return (
     <div className="bg-[#FFA1BF] min-h-screen overflow-x-hidden z-20 md:max-w-[1366px]">
       <div className="relative p-2">
-        <img src={HEROs} alt="" className="w-full h-[500px] lg:h-screen object-contain lg:object-cover" />
+        <img
+          src={HEROs}
+          alt=""
+          className="w-full h-[500px] lg:h-screen object-contain lg:object-cover"
+        />
         <div className="absolute inset-x-0 -bottom-[30%] md:-bottom-[32%] md:flex justify-between hidden">
-          <img src={HERO1} alt="" className="transform -translate-x-24 h-[400px] lg:flex hidden" />
+          <img
+            src={HERO1}
+            alt=""
+            className="transform -translate-x-24 h-[400px] lg:flex hidden"
+          />
           <img
             src={HERO2}
             alt=""
             className="transform translate-y-24 object-contain rounded-3xl h-[150px] md:h-[350px]"
           />
-          <img src={HERO3} alt="" className="transform translate-x-24 h-[350px] lg:flex hidden" />
+          <img
+            src={HERO3}
+            alt=""
+            className="transform translate-x-24 h-[350px] lg:flex hidden"
+          />
         </div>
       </div>
       <div className="py-10 bg-[#FF3DB4] px-6 lg:px-20 pt-[100px] md:pt-[350px]">
         <div className="grid lg:grid-cols-2 gap-5 justify-items-center items-center">
-          <img src={SECTION_1} alt="" className="md:h-[450px] h-[350px] object-contain lg:object-cover" />
+          <img
+            src={SECTION_1}
+            alt=""
+            className="md:h-[450px] h-[350px] object-contain lg:object-cover"
+          />
           <p className="text-3xl px-5 lg:px-0 lg:text-4xl text-center text-white leading-snug">
             Welcome to Hello Kitty World, the ultimate meme project on Solana!
             Enter a playful universe where our adorable leader, Hello Kitty,
@@ -50,7 +66,11 @@ const Home = () => {
             chaos. Get ready for laughter, fun, and moon-worthy gains. Embrace
             the new world order, where Hello Kitty rules!
           </p>
-          <img src={SECTION_2} alt="" className="h-[450px] object-contain md:col-start-2 md:col-end-3 order-2" />
+          <img
+            src={SECTION_2}
+            alt=""
+            className="h-[450px] object-contain md:col-start-2 md:col-end-3 order-2"
+          />
         </div>
       </div>
       <div className="bg-[#FF3DB4] py-10 flex flex-col items-center pt-[100px] md:pt-[200px]">
@@ -86,7 +106,11 @@ const Home = () => {
           />
         </div>
         <div className="pt-10 grid lg:grid-cols-2 gap-5 justify-items-center items-center px-6">
-          <img src={TIMELINESIX} alt="timeline" className="object-contain h-[350px]" />
+          <img
+            src={TIMELINESIX}
+            alt="timeline"
+            className="object-contain h-[350px]"
+          />
           <p className="text-2xl lg:text-4xl text-center text-white leading-snug w-[350px] md:w-full">
             Join us in creating a new world order with Kitty leading the way!
             🚀🔥😺
@@ -95,14 +119,22 @@ const Home = () => {
       </div>
       <div className="bg-[#FF3DB4] pt-10 flex flex-col items-center px-6 lg:px-20 pb-10 text-white">
         <div className="relative w-full">
-          <img src={GLASS} alt="background-footer" className="w-full h-[500px] md:h-64" />
+          <img
+            src={GLASS}
+            alt="background-footer"
+            className="w-full h-[500px] md:h-64"
+          />
           <div className="absolute inset-0 flex flex-col">
             <div className="grid w-full h-full grid-cols-1 lg:grid-cols-3 gap-5 py-8 px-6 lg:px-10">
               <div className="flex flex-col items-center">
                 <h2 className="text-3xl font-bold">$KITTY</h2>
                 <div className="flex py-2 space-x-2">
-                  <FaTwitter />
-                  <FaTelegram />
+                  <a href="https://x.com/hellokittyspl?s=21">
+                    <FaTwitter />
+                  </a>
+                  <a href="https//t.me/HelloKittyWorldSPL" target="_blank">
+                    <FaTelegram />
+                  </a>
                   {/* <FaLinkedin /> */}
                 </div>
               </div>
@@ -118,9 +150,9 @@ const Home = () => {
               </div>
               <div className="grid grid-cols-2 gap-2">
                 <div className="flex flex-col">
-                  <h2>Tokenomics</h2>
+                  {/* <h2>Tokenomics</h2> */}
                   <h2>Team Section</h2>
-                  <h2>About US</h2>
+                  {/* <h2>About US</h2> */}
                 </div>
                 <div className="flex flex-col">
                   <h2>Github</h2>
@@ -140,7 +172,11 @@ const Home = () => {
           </div>
         </div>
         <div className="hidden md:relative w-full h-[200px]">
-          <img src={FOOTER} alt="" className="object-contain h-[150px] w-[150px] absolute right-0 bottom-[65px]" />
+          <img
+            src={FOOTER}
+            alt=""
+            className="object-contain h-[150px] w-[150px] absolute right-0 bottom-[65px]"
+          />
         </div>
       </div>
     </div>
